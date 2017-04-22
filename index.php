7f982e8adef647b212745923e0008741d91e5b84
Bruno Arakaki <brn.arakaki@gmail.com>
2017-04-22 15:49:42 -0700

    test php connection with db

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html");
+
+
+$servername = "104.198.224.126";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$conn->close();
+?>
