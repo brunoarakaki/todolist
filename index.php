43badad53a6b8fbfc98360bd47d82d1c6f84d81a
Bruno Arakaki <brn.arakaki@gmail.com>
2017-04-22 16:03:19 -0700

    testing php select

--- index.php
+++ index.php
@@ -4,14 +4,24 @@
 $servername = "104.198.224.126";
 $username = "root";
 $password = "root";
+$dbname = "todo";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "SELECT id, description FROM todo";
+$result = $conn->query($sql);
+
+
+while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Description: " . $row["description"]. "<br>";
+}
+
 $conn->close();
 ?>
